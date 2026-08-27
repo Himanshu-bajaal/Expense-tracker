4a66dfba7bfa3f315077219d0a47bf9e08eb4b1b
Himanshu-bajaal <himanshubajaal@gmail.com>
2026-08-27 13:18:46 +0530

    added useeffect to localy store values, and added deleter feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ExpenseForm from "./components/ExpenseForm"
 import ExpenseList from "./components/ExpenseList";
 import ExpenseTotal from "./components/ExpenseTotal";
 import './App.css'
 
 function App() {
-  const [expenses, setExpenses] = useState([])
+  const [expenses, setExpenses] = useState(()=> {
+    const saved = localStorage.getItem('expenses')
+    return saved? JSON.parse(saved) : [] 
+  })
+
+  useEffect(()=>{
+    localStorage.setItem('expenses',JSON.stringify(expenses))
+  },[expenses])
 
   function handleAddExpense(newExpense) {
     setExpenses([...expenses, newExpense])
+  }
+
+  function handleDeleteExpense(id) {
+    setExpenses(expenses.filter((expense) => expense.id !== id))
   }
 
   return (
@@ -16,7 +27,7 @@ function App() {
       <h1>Expense tracker</h1>
       
       <ExpenseForm onAddExpense={handleAddExpense} />
-      <ExpenseList expenses={expenses} />
+      <ExpenseList expenses={expenses} onDeleteExpense={handleDeleteExpense} />
       <ExpenseTotal expenses={expenses}/>
     </div>
   )
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,9 +1,13 @@
-function ExpenseList({expenses}){
+function ExpenseList({expenses, onDeleteExpense}){
+    function handleDelete(id){
+        onDeleteExpense(id)
+    }
     return(
         <ul>
             {expenses.map((expense)=>(
                 <li key={expense.id}>
-                    {expense.description} - {expense.amount}
+                    {expense.description} - {expense.amount} 
+                    <button type="submit"  onClick={()=>handleDelete(expense.id)}> Delete</button>
                 </li>
             ))}
         </ul>
